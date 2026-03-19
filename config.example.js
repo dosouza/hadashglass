f62799903a40ce1b4f6f00a43641cc1e15a366ef
Demetrius <dosouzasp@gmail.com>
2026-03-18 21:43:50 -0300

    Criação do arquivo .gitignore e do config.example.js

---
+++ config.example.js
@@ -0,0 +1,8 @@
+// Renomeie para config.js e preencha seus dados
+export const HA_CONFIG = {
+    URL: "http://SEU_IP:8123",
+    TOKEN: "SEU_TOKEN_AQUI"
+};
+export const WEATHER_ENTITY = "weather.sua_entidade";
+export const RAIN_DAY_1 = "sensor.sua_entidade_chuva";
+export const RAIN_NIGHT_1 = "sensor.sua_entidade_chuva_noite";
